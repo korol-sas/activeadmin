42e7aeaddcdb80a249527d0e1ade6f2e165cebaa
Piers Chambers <piers@varyonic.com>
2017-03-21 20:21:52 -0400

    Rails 5 changed belongs_to from optional to required by default.

--- spec/support/rails_template.rb
+++ spec/support/rails_template.rb
@@ -139,6 +139,10 @@ gsub_file 'config/environments/test.rb', /  config.cache_classes = true/, <<-RUB
     config.active_record.maintain_test_schema = false
   end
 
+  if Rails::VERSION::MAJOR >= 5
+    config.active_record.belongs_to_required_by_default = false
+  end
+
 RUBY
 
 # Add our local Active Admin to the application
